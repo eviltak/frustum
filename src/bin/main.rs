3ea8c4ac8bb45ddc713bec450a036b687f46d846
Arav Singhal <singhal.arav@gmail.com>
2016-11-28 10:57:58 +0530

    Add file adding methods to Session
     * Add small tests to main executable. Will be moved to proper tests.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,4 +2,11 @@ extern crate frustum;
 
 fn main() {
     println!("Hello, frustum!");
+
+    // TODO: Move tests to tests/*.rs
+    let session = frustum::Session::new();
+    session.add_file("lib.rs".to_string(), "mod module;".to_string());
+
+    println!("{:?}",
+             session.parse_sess.codemap().files.borrow_mut().len());
 }
--- src/session.rs
+++ src/session.rs
@@ -1,6 +1,25 @@
 
 // Copyright (c) 2016 Arav Singhal.
 
+use syntax;
+
 pub struct Session {
     // TODO: Complete definition
+    pub parse_sess: syntax::parse::ParseSess,
+}
+
+impl Session {
+    pub fn new() -> Session {
+        Session { parse_sess: syntax::parse::ParseSess::new() }
+    }
+
+    pub fn new_from_file(name: String, source: String) -> Session {
+        let session = Session::new();
+        session.add_file(name, source);
+        session
+    }
+
+    pub fn add_file(&self, name: String, source: String) {
+        self.parse_sess.codemap().new_filemap(name, None, source);
+    }
 }
